6d0bbeb15594d8f46a8244313eaec3aeb0f79013
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-16 19:26:21 +0530

    Deleting the stack Index

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//onsole.WriteLine("Hello, World!");
+using LinkedListStackUC2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkedListStack linkedListStack = new LinkedListStack();
+        linkedListStack.Push(70);
+        linkedListStack.Push(30);
+        linkedListStack.Push(56);
+        linkedListStack.Display();
+        linkedListStack.Peek();
+        linkedListStack.Pop();
+        linkedListStack.IsEmpty();
+        
+
+    }
+}
