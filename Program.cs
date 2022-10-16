0d37066aee68bf57cd16122f4f2adbec6f4fd262
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-16 19:13:09 +0530

    Adding three index using LinkedList

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using LinkedList_UC1;
+using System;
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linked a = new Linked();
+            a.Add(56);
+            a.Add(30);
+            a.Add(70);
+            a.Display();
+
+
+        }
+    }
+}
