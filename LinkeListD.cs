e37fb8d668035944d1a3fa552e13c56101c6c460
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-16 19:29:18 +0530

    Dequeue the element

---
+++ LinkeListD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkjedListQueueUC4
+{
+    internal class LinkeListD
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in Queue", node.data);
+
+        }
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("Dequeued  value is {0}", this.head.data);
+                    this.head = this.head.next;
+                }
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LinkjedListQueueUC4;
+using System;
+class program
+{
+    public static void Main(string[] args)
+    {
+        LinkeListD a = new LinkeListD();
+        a.Enqueue(56);
+        a.Enqueue(30);
+        a.Enqueue(70);
+        a.Display();
+        a.Dequeue();
+    }
+}
+
