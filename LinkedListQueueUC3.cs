3ee511683a111bcccfc2553d57cee43d87b48c4a
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-16 19:27:34 +0530

    LinkedListQueue

---
+++ LinkedListQueueUC3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListQueue
+{
+    internal class LinkedListQueueUC3
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next !=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in Queue", node.data);
+
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LinkedListQueue;
+using System;
+class program
+{
+    public static void Main(string[] args)
+    {
+        LinkedListQueueUC3 a = new LinkedListQueueUC3();
+        a.Enqueue(56);
+        a.Enqueue(30);
+        a.Enqueue(70);
+        a.Display();
+    }
+}
