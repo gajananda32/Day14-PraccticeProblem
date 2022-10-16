17239b2ef6f6a1e7f40e66f5cef22c4daa5257ca
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-16 19:24:51 +0530

    Adding LinkedListStack

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using LinkedListStackUC1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkedlistStackUC1 linkedListStack = new LinkedlistStackUC1();
+        linkedListStack.Push(70);
+        linkedListStack.Push(30);
+        linkedListStack.Push(56);
+        linkedListStack.Display();
+
+    }
+}
